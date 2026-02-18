425a7638a63b412cb27b76b22a582b51ccb52456
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-02-18 10:37:56 +0600

    login

--- script/login.js
+++ script/login.js
@@ -1,1 +1,18 @@
-console.log('login in your page');
+document.getElementById('login-btn').addEventListener('click', function () {
+  //   get the number input
+  const numberInput = document.getElementById('input-number');
+  const contactNumber = numberInput.value;
+  console.log(contactNumber);
+  //   get pin input
+  const pinInput = document.getElementById('input-pin');
+  const pinValue = pinInput.value;
+  console.log(pinValue);
+  //   match number & pin
+  if (contactNumber == '01234567890' && pinValue == '1234') {
+    alert('login succesfull');
+    window.location.assign('/DOM-project/home.html');
+  } else {
+    alert('login failed');
+    return;
+  }
+});
